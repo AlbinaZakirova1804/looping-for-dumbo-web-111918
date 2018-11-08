a1ff215c16e913a63c60ff9237eb935297a1b125
Albina Zakirova <zak1804@gmail.com>
2018-11-08 04:08:05 +0000

    automatically backed up by learn

--- for.rb
+++ for.rb
@@ -1,8 +1,9 @@
 
 def using_for
 	checklist = 1..10
-	for counter in checklist do
- puts "Wingardium Leviosa"	#your code here
+  for ithem in checklist do
+   puts "Wingardium Leviosa"	#your code here
+  end
 end
 
 
